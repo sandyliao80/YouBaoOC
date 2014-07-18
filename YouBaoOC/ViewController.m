056175c6d30d0d8c817b568edb0b629b69e3ed5d
icylydia <icylydia@gmail.com>
2014-07-18 17:00:42 +0800

    init and add some pods

---
+++ YouBaoOC/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  YouBaoOC
+//
+//  Created by eagle on 14/7/18.
+//  Copyright (c) 2014年 Duostec. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
