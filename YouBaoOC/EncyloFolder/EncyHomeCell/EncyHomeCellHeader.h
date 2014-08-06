cfcc001dcaddb80e6398380aa137524a42ef6e37
ChinaPicture <576414408@qq.com>
2014-08-06 12:46:38 +0800

    修改

---
+++ YouBaoOC/EncyloFolder/EncyHomeCell/EncyHomeCellHeader.h
@@ -0,0 +1,17 @@
+//
+//  EncyHomeCellHeader.h
+//  YouBaoOC
+//
+//  Created by developer on 14-8-6.
+//  Copyright (c) 2014年 Duostec. All rights reserved.
+//
+
+#ifndef YouBaoOC_EncyHomeCellHeader_h
+#define YouBaoOC_EncyHomeCellHeader_h
+#import "EncyHomeImageCell.h"
+#import "EncyHomeTitleCell.h"
+#import "EncyHomePageTableViewCell.h"
+#define EN_HOMEIMAGECELLIDENTIFIER @"EncyHomeImageCell"
+#define EN_HOMETITLECELLIDENTIFIER @"EncyHomeTitleCell"
+#define EN_HOMEPAGECELLIDENTIFIER  @"EncyHomeIdentifier"
+#endif
---
+++ YouBaoOC/EncyloFolder/EncyHomeCell/EncyHomeTitleCell.m
@@ -0,0 +1,31 @@
+//
+//  EncyHomeTitleCell.m
+//  YouBaoOC
+//
+//  Created by developer on 14-8-6.
+//  Copyright (c) 2014年 Duostec. All rights reserved.
+//
+
+#import "EncyHomeTitleCell.h"
+
+@implementation EncyHomeTitleCell
+
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
